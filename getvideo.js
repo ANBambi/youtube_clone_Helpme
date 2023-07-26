c9d1d330d0db0b50a8cc68e4db79ccdb9e9dd4f6
jjm0521 <wkdwhdals05@nabver.com>
2023-07-26 17:07:56 +0900

    230726 장종민 js1 담당 html의  js링크 및 dom추가 // getvideo.js 및 communication.js 추가

--- getvideo.js
+++ getvideo.js
@@ -80,18 +80,51 @@ async function displayVideo(id) {
         // 비디오 정보를 표시할 문자열 생성
         let channelURL = `location.href="./channel.html"`;
         let videoURL = `location.href="./video.html?id=${videoId}"`;
-
-        if (id == videoId){
+        let num = videoInfo.views;            //숫자값 format
+        if (id == videoId){                   //comment 수정 필요
+            num = formatNumberWithCommas(num);
             videoHTML = `
-            <video controls style='width:800px'>
-                <source src='${videoInfo.video_link}'>
-            </video>    
+            <div>
+                <video controls style='width:800px'>
+                    <source src='${videoInfo.video_link}'>
+                </video>
+                <br>
+                <p style='font-size:24px;padding:15px;'>${videoInfo.video_title}</p>
+                <div style='display:flex;justify-content: space-between; padding:15px;'>
+                    <div>
+                        <p>${num} views ${videoInfo.upload_date}</p>
+                    </div>
+                    <div>
+                        <img src='videoImg/thumb_up.svg'><span>좋아요</span>
+                        <img src='videoImg/thumb_down.svg'><span>싫어요</span>
+                        <img src='videoImg/Share.svg'><span>공유</span>
+                        <img src='videoImg/Save.svg'><span>저장</span>
+                        <img src='videoImg/More.svg'><span>더보기</span>
+                    </div>
+                </div>
+                <br>
+                <div style='display:flex;justify-content: space-between; padding:15px;'>
+                    <div style='display:flex;'>
+                        <div style='width:50px; height: 50px; border-radius: 70%; overflow:hidden;'>
+                            <img src='imgImg/Profile.svg' style='width:100%; height:100%; object-fit:cover; cursor:pointer;' onclick='${channelURL}'></img>
+                        </div>
+                        <div style='margin:10px'>
+                            <p>oreumi</p>
+                            <br>
+                            <p>comment</p>
+                        </div>
+                    </div>
+                    <div>
+                        <img src='/img/channel/Subscribes-Btn.png'>
+                    </div>
+                </div>
+            </div>    
             `;
         }else{
             listHTML += `
-            <div>
+            <div style="display:flex;">
                 <img src='${videoInfo.image_link}' style='width:320px;cursor:pointer;' onclick='${videoURL}'></img>
-                <div style='display:flex;'>
+                <div>
                     <div>
                         <p>${videoInfo.video_title}</p>
                         <p>${videoInfo.video_channel}</p>
@@ -127,9 +160,24 @@ async function displayChannel() {
         const videoId = videoInfo.video_id;
 
         // 비디오 정보를 표시할 문자열 생성
-        let videoURL = `location.href="./video.html?id=${videoId}"`;
-
-        infoHTML += `
+        let videoURL = `location.href="./index_video.html?id=${videoId}"`;
+        let num = videoInfo.views;
+        if (i===1){// comment 수정 필요.
+            num = formatNumberWithCommas(num);
+            smalHTML = `
+            <div>
+                <video controls style='width:500px'>
+                    <source src='${videoInfo.video_link}'>
+                </video>
+            </div>
+            <div>
+                <p>${videoInfo.video_title}</p>
+                <p>${num}views ${videoInfo.upload_date}</p>
+                <p>comment</p>
+            </div>
+            `;
+        }else{
+            infoHTML += `
             <div>
                 <img src='${videoInfo.image_link}' style='width:320px;cursor:pointer;' onclick='${videoURL}'></img>
                 <div>
@@ -140,9 +188,12 @@ async function displayChannel() {
                         <p>${videoInfo.upload_date}</p>
                     </div>
                 </div>
-            </div>
-        `;
-    }
-
-    infoContainer.innerHTML = infoHTML;
-}
+                </div>
+                `;
+                }
+            }
+            smalVideo.innerHTML = smalHTML;
+            infoContainer.innerHTML = infoHTML;
+        }
+        
+        
